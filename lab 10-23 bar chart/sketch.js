2bb5783b3ee06fc1b5b06494204ed824ae300c31
Laura Quintana <795085@seq.org>
2019-10-24 17:07:31 -0700

    Update sketch.js

--- lab 10-23 bar chart/sketch.js
+++ lab 10-23 bar chart/sketch.js
@@ -1,31 +1,38 @@
-var bar = [random(0,10000)]
+var bar = [0,2,5,1];
 var n = 0;
+var x, y;
+var i, j;
 function setup() {
 
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
-  fill(200, 30, 150);
 
-  loadBars();
+
   insertionSort();
 
 }
 //swap code
 
-function loadBars(){
-  bar = new Bar(width, height, random(-800,800), random(-800,800));
-}
 
-function insertionSort(n){
-  for(i=0; i> bar.length; i++){
-    for(j=i; j > 0; j++){
-      if(bar[j-1] > bar[j]){
-        swap(bar[j] , bar[j+1]);
+
+function insertionSort(){
+  for(i=0; i < bar.length; i++){
+    for(j=i; j > 0; j--){
+      if(bar[j-1] < bar[j]){
         var temp = bar[j];
-         bar[j]= bar[j+1];
-         bar[j+1] = temp;
+         bar[j-1]= bar[j];
+         bar[j-1] = temp;
+         drawBars();
       }
     }
+  }
+}
+
+function drawBars(){
+  for(i = 0; i< bar.length; i++){
+    var w=3;
+    var bar = new Bar(i*3, 0,w,bar[i]);
+    bar.render;
   }
 }
