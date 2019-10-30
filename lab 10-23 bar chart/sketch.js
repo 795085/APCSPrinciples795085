beeb90e41816f269003b3391e3d18c7f93e2ce2d
Laura Quintana <lquintana@WS-ACADBUS.EDU>
2019-10-30 08:51:52 -0700

    new changes bar sort

--- lab 10-23 bar chart/sketch.js
+++ lab 10-23 bar chart/sketch.js
@@ -8,48 +8,45 @@ function setup() {
 
 barWidth = 20;
 n = width/ barWidth;
-  loadBars(n);
-  drawBars();
 
-  runBars();
-  insertionSort();
-
-
-}
-function drawBars(){
-  background(5, 5, 5);
-  frameRate(1);
-  runBars();
-}
-//swap code
-//vector located here
-function loadBars(n){
+//loading bars
 for(var i = 0;  i < n; i++){
   var barHeight = Math.floor(random(height));
   var loc = createVector(i* barWidth, barHeight);
   bar[i]= new Bar( loc , height - barHeight);
   }
+  //functions called
+  drawBars();
 }
 
 //run bars
-function runBars(){
+
+function drawBars(){
+  background(5, 5, 5);
+  frameRate(1);
+
+//runs bars
   for(var i = 0; i < bar.length;i++){
     bar[i].render();
   }
-  console.log(bar[i]);
-}
-
-//sorting algorithm
-function insertionSort(n){
-  for(i=0; i< bar.length; i++){
-    for(var j=i; j > 0; j--){
+//loop insertion sorting bars
+   //for(i=0; i< bar.length; i++){
+    for(var j=0; j > 0; j--){
       if(bar[j-1] > bar[j]){
         var temp = bar[j];
          bar[j-1]= bar[j];
          bar[j-1] = temp;
       }
     }
-  }
- console.log(bar[i]);
+  // }
+console.log(bar[i]);
  console.log(bar[j]);
 }
+
+
+//swap code
+//vector located here
+
+//sorting algorithm
+
+  //
